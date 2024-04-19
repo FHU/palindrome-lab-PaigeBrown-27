5540ab8116393547da918a733b5dd9a8369edc14
PaigeBrown-27 <paige.brown@students.fhu.edu>
2024-04-19 09:58:44 -0500

    tacocat

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    lower_case = word.lower()
+    remove_space = lower_case.replace(" ", "")
+    first_half = remove_space[:len(remove_space)//2]
+    second_half = remove_space[len(remove_space)//2]
+    if remove_space == "":
+        return False
+    elif first_half[0] == second_half[-1]:
+       return True
+    else:
+        return False 
 
 if __name__ == '__main__': 
-    #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    user_input = input()
+    print(palindrome(user_input))
+
